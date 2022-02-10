adb00a8528c46bfae21d7331c0766321fa2c32a0
Jay Shin <sjwy2k@gmail.com>
2022-02-10 13:51:08 +0900

    200210 5장 HttpRequest java파일 작성 HttpRequest : 유저의 요청을 다루는 파일 processRequestLine : request line을 읽고 요청을 분리해 처리 getMethod, getPath, getHeader, getParameter

--- src/test/java/util/HttpRequest.java
+++ src/test/java/util/HttpRequest.java
@@ -1,31 +1,87 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpRequest {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+	
+	private String method;
+	private String path;
+	private Map<String, String> headers = new HashMap<String, String>();
+	private Map<String, String> params = new HashMap<String, String>();
+	
 	public HttpRequest(InputStream in) {
-		// TODO Auto-generated constructor stub
+		try {
+			BufferedReader br = 
+					new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
+			if (line == null) {
+				return;
+			}
+			
+			processRequestLine(line);
+			
+			line = br.readLine();
+			while (!line.equals("")) {
+				log.debug("header : {}", line);
+				String[] tokens =  line.split(":");
+				headers.put(tokens[0].trim(), tokens[1].trim());
+				line = br.readLine();
+			}
+			
+			if ("POST".equals(method)) {
+				String body = 
+						IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+				params = HttpRequestUtils.parseQueryString(body);
+			}
+		} catch (IOException io) {
+			log.error(io.getMessage());
+		}
 	}
 
-	public Object getMethod() {
-		// TODO Auto-generated method stub
-		return null;
+	private void processRequestLine(String requestLine) {
+		log.debug("request line : {}", requestLine);
+		String[] tokens = requestLine.split(" ");
+		method = tokens[0];
+		
+		if ("POST".equals(method)) {
+			path = tokens[1];
+			return;
+		}
+		
+		int index = tokens[1].indexOf("?");
+		if (index == -1) {
+			path = tokens[1];
+		} else {
+			path = tokens[1].substring(0, index);
+			params = HttpRequestUtils.parseQueryString(
+					tokens[1].substring(index + 1));					
+		}
 	}
 
-	public Object getPath() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getMethod() {
+		return method;
 	}
 
-	public Object getHeader(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getPath() {
+		return path;
 	}
 
-	public Object getParameter(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getHeader(String name) {
+		return headers.get(name);
+	}
+
+	public String getParameter(String name) {
+		return params.get(name);
 	}
 
 }
